7c7cd81b21a6f3022fbb7a6e3a8a624bd4fd8eb9
SelvaKrishnan1103 <selvakrish601@gmail.com>
2025-10-27 16:21:16 +0530

    classify added

--- src/app/Components/Section/upload.jsx
+++ src/app/Components/Section/upload.jsx
@@ -75,7 +75,7 @@ export default function Upload() {
       setLoading(true);
       setError('');
 
-      const classifyRes = await fetch('https://selva1103-eduhush-classify.hf.space/', {
+      const classifyRes = await fetch('https://selva1103-eduhush-classify.hf.space/classify', {
         method: 'POST',
         body: formData,
       });
