1ff83541b373cb09bfa932aaf061b428f6f8c269
Selvakrishnan1103 <selvakrish601@gmai.com>
2025-10-28 17:05:50 +0530

    files added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
-'use client'; 
-
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
+import Providers from "./Providers";
 
+export const metadata = {
+  title: "EduHush",
+  description: "A study-based video platform",
+  icons: {
+    icon: "/favicon_edu_v2.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-        <SessionProvider>{children}</SessionProvider>
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
